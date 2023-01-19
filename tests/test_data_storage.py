5b9f7984a2ce2ac7e1283137f4ae47888197c4cd
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-18 23:03:35 -0500

    Create test_data_storage.py

---
+++ tests/test_data_storage.py
@@ -0,0 +1,7 @@
+import unittest
+from my_web_scraper.data_storage import DataStorage
+
+class TestDataStorage(unittest.TestCase):
+    def test_to_csv(self):
+        # Add test cases here
+        pass
