cba84c53c560068762ce0e385466fd3c9825f958
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-18 22:55:42 -0500

    Create example_usage.py

---
+++ examples/example_usage.py
@@ -0,0 +1,21 @@
+from my_web_scraper.scraper import Scraper
+from my_web_scraper.parser import Parser
+from my_web_scraper.data_storage import DataStorage
+
+# Create an instance of the Scraper class
+scraper = Scraper("https://www.who.int/covid-19/")
+
+# Get the HTML from the website
+html = scraper.get_html()
+
+# Create an instance of the Parser class
+parser = Parser(html)
+
+# Extract the data from the HTML
+data = parser.get_data()
+
+# Create an instance of the DataStorage class
+data_storage = DataStorage(data, "covid19_data.csv")
+
+# Save the data to a CSV file
+data_storage.to_csv()
